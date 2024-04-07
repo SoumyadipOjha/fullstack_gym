dc980163a6f7a60af4203bd45cdd2e963e8f3d21
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-04-07 12:30:46 +0530

    added entry

--- login.js
+++ login.js
@@ -1,61 +1,3 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var mongoose = require("mongoose");
-
-// const app = express();
-
-// app.use(bodyParser.json());
-// app.use(express.static("./")); //public changed to /
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
-
-// mongoose.connect("mongodb://localhost:27017/Database");
-// var db = mongoose.connection;
-// db.on("error", () => console.log("Error in Connecting to Database"));
-// db.once("open", () => console.log("Connected to Database"));
-
-// app.post("/sign_up", (req, res) => {
-//   var name = req.body.name;
-//   var age = req.body.age;
-//   var email = req.body.email;
-//   var phno = req.body.phno;
-//   var gender = req.body.gender;
-//   var password = req.body.password;
-
-//   var data = {
-//     name: name,
-//     age: age,
-//     email: email,
-//     phno: phno,
-//     gender: gender,
-//     password: password,
-//   };
-//   db.collection("users").insertOne(data, (err, collection) => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log("Record Inserted Succesfully");
-//   });
-//   return res.redirect("signup_successful.html");
-// });
-
-// // app.get("/", (req, res) => {
-// //     res.set({
-// //       "Allow-acces-Allow-Origin": "*",
-// //     });
-// //     return res.redirect("loginReg.html");
-// //   })
-// //   .listen(3030);
-// app
-//   .get("/", (req, res) => {
-//     const filePath = path.join(__dirname, "loginReg.html");
-//     res.sendFile(filePath);
-//   })
-//   .listen(3030);
-// console.log("Listening on port 3030");
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
@@ -64,48 +6,56 @@ var path = require("path");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static("./")); // public changed to /
+app.use(express.static("./"));
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 
-mongoose.connect("mongodb+srv://soumyadipojha635:WZJOfLt8Cbf3rwbY@cluster0.njhxvfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb://localhost:27017/Database");
+mongoose.connect("mongodb://localhost:27017/uersdetails", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 var db = mongoose.connection;
-db.on("error", () => console.log("Error in Connecting to Database"));
+db.on("error", (err) => console.log("Error in Connecting to Database: " + err));
 db.once("open", () => console.log("Connected to Database"));
 
 app.post("/sign_up", (req, res) => {
-  var name = req.body.name;
-  var age = req.body.age;
-  var email = req.body.email;
-  var phno = req.body.phno;
-  var gender = req.body.gender;
-  var password = req.body.password;
+  try {
+    var name = req.body.name;
+    var age = req.body.age;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var gender = req.body.gender;
+    var password = req.body.password;
 
-  var data = {
-    name: name,
-    age: age,
-    email: email,
-    phno: phno,
-    gender: gender,
-    password: password,
-  };
-  db.collection("users").insertOne(data, (err, collection) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Record Inserted Succesfully");
-  });
-  return res.redirect("dashboard.html");
+    var data = {
+      name: name,
+      age: age,
+      email: email,
+      phno: phno,
+      gender: gender,
+      password: password,
+    };
+    db.collection("users").insertOne(data, (err, collection) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Record Inserted Successfully");
+      res.redirect("gymvisit.html");
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: e.message });
+  }
 });
 
 app.get("/loginReg", (req, res) => {
-  const filePath = path.join(__dirname, "dashboard.html");
+  const filePath = path.join(__dirname, "loginReg.html");
   res.sendFile(filePath);
 });
 
-app.listen(3030, () => {
-  console.log("Listening on port 3030");
+app.listen(5000, () => {
+  console.log("Listening on port 5000");
 });
